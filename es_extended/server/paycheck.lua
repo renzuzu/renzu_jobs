81fb696e60dc7a4dd91614c5466f7212917024c2
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-09-29 12:51:29 +0800

    tweak(files): paycheck files

---
+++ es_extended/server/paycheck.lua
@@ -0,0 +1,35 @@
+ESX.StartPayCheck = function()
+	function payCheck()
+		local xPlayers = ESX.GetPlayers()
+
+		for i=1, #xPlayers, 1 do
+			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+			local job     = xPlayer.job.grade_name
+			local salary  = xPlayer.job.grade_salary
+
+			if salary > 0 then
+				if job == 'unemployed' then -- unemployed
+					xPlayer.addAccountMoney('bank', salary)
+					TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_help', salary), 'CHAR_BANK_MAZE', 9)
+				elseif Config.EnableSocietyPayouts then -- possibly a society
+					if tonumber(exports.renzu_jobs:JobMoney(job).money) >= salary then -- does the society money to pay its employees?
+						xPlayer.addAccountMoney('bank', salary)
+						removemoney = exports.renzu_jobs:removeMoney(tonumber(salary),job,source,'money',true)
+
+						TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_salary', salary), 'CHAR_BANK_MAZE', 9)
+					else
+						TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), '', _U('company_nomoney'), 'CHAR_BANK_MAZE', 1)
+					end
+				else -- generic job
+					xPlayer.addAccountMoney('bank', salary)
+					TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_salary', salary), 'CHAR_BANK_MAZE', 9)
+				end
+			end
+
+		end
+
+		SetTimeout(Config.PaycheckInterval, payCheck)
+	end
+
+	SetTimeout(Config.PaycheckInterval, payCheck)
+end
---
+++ es_extended/server/paycheck_LEGACY.lua
@@ -0,0 +1,32 @@
+StartPayCheck = function()
+	Citizen.CreateThread(function()
+		while not GetResourceState('renzu_jobs') do Wait(1000) end
+		while true do
+			Citizen.Wait(Config.PaycheckInterval)
+			local xPlayers = ESX.GetExtendedPlayers()
+			for _, xPlayer in pairs(xPlayers) do
+				local job     = xPlayer.job.grade_name
+				local salary  = xPlayer.job.grade_salary
+
+				if salary > 0 then
+					if job == 'unemployed' then -- unemployed
+						xPlayer.addAccountMoney('bank', salary)
+						TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_help', salary), 'CHAR_BANK_MAZE', 9)
+					elseif Config.EnableSocietyPayouts then -- possibly a society
+						if tonumber(exports.renzu_jobs:JobMoney(job).money) >= salary then -- does the society money to pay its employees?
+							xPlayer.addAccountMoney('bank', salary)
+							removemoney = exports.renzu_jobs:removeMoney(tonumber(salary),job,source,'money',true)
+
+							TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_salary', salary), 'CHAR_BANK_MAZE', 9)
+						else
+							TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), '', _U('company_nomoney'), 'CHAR_BANK_MAZE', 1)
+						end
+					else -- generic job
+						xPlayer.addAccountMoney('bank', salary)
+						TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_salary', salary), 'CHAR_BANK_MAZE', 9)
+					end
+				end
+			end
+		end
+	end)
+end
